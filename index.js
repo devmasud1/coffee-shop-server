d84ac722eeee589701f43756d3cb1eba440afd29
devmasud1 <devmahmud.masu1@gmail.com>
2023-10-15 15:03:27 +0600

    create basic setup & post a doc & get all doc

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+//middleware
+
+app.get("/", (req, res) => {
+  res.send("welcome to coffee shop");
+});
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.aunb3y8.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const coffeesCollection = client.db("coffeesDB").collection("coffees");
+
+    app.get("/coffees", async (req, res) => {
+      const cursor = coffeesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/coffee", async (req, res) => {
+      const newCoffee = req.body;
+      const result = await coffeesCollection.insertOne(newCoffee);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
